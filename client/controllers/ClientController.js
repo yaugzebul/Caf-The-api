53152ee267a44f41e0e1bef03f3fdea0724fc1ea
yaugzebul <romain.testaert@campus-centre.fr>
2026-03-16 15:39:47 +0100

    changement du cookie en secure:true

--- client/controllers/ClientController.js
+++ client/controllers/ClientController.js
@@ -86,7 +86,7 @@ const login = async (req, res) => {
         )
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: "lax",
             maxAge: expire * 1000,
         });
